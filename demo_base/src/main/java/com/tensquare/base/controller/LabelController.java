942364a358299eee18ec42562f1522223aa18e65
WeiChongLu <599272685@qq.com>
2019-04-09 22:09:26 +0800

    3

--- demo_base/src/main/java/com/tensquare/base/controller/LabelController.java
+++ demo_base/src/main/java/com/tensquare/base/controller/LabelController.java
@@ -42,6 +42,7 @@ public class LabelController {
                 address +"-----" + request.getServerPort() );
         System.out.println("111111");
         System.out.println("222222");
+        System.out.println("333333");
         return new Result(true, StatusCode.OK,"查询成功",labelService.findAll());
 
     }
